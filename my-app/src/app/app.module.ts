79e623e23c8141fdba7e84740b7b2090e1b56f31
TehBuckets <44855101+TehBuckets@users.noreply.github.com>
2020-05-08 02:02:24 +0200

    Revert "Modules added"
    This reverts commit 484cda12f9e033df3217ca55697d637db1be041b.

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,13 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule, JsonpModule } from '@angular/http';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { BrowserDomAdapter } from '@angular/platform-browser/src/browser/browser_adapter';
-import { BrowserJsonp } from '@angular/http/src/backends/browser_jsonp';
-import { GenericBrowserDomAdapter } from '@angular/platform-browser/src/browser/generic_browser_adapter';
-import { JsonPipe } from '@angular/common/src/pipes/json_pipe';
 
 @NgModule({
   declarations: [
@@ -16,14 +12,7 @@ import { JsonPipe } from '@angular/common/src/pipes/json_pipe';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    BrowserDomAdapter,
-    BrowserJsonp,
-    JsonpModule,
-    JsonPipe,
-    Animation,
-    AnimationEffect,
-    AnimationEvent,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
